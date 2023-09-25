643b0ce23d96924e793c66485f008f504fe2dd14
Izzat <izzat980420@example.com>
2023-09-26 00:00:17 +0800

    update from page

--- app/component/hero/Hero2.tsx
+++ app/component/hero/Hero2.tsx
@@ -9,8 +9,8 @@ import { AiOutlineWhatsApp } from "react-icons/ai";
 function Hero2() {
   return (
     <main>
-      <div className="relative overflow-hidden">
-        <div className=" relative mx-auto  h-[520px]">
+      <div className=" overflow-hidden">
+        <div className=" relative mx-auto  h-[520px] md:h-auto">
           <div>
             <Image
               src={Background}
@@ -22,38 +22,53 @@ function Hero2() {
               <Image
                 src={Doctor}
                 alt=""
-                className="absolute bottom-0 -right-10 h-[350px] w-auto"
-                // layout="fill"
-                // objectFit="cover"
+                className="absolute bottom-0 -right-10 h-[350px] w-auto md:hidden"
               ></Image>
             </div>
           </div>
-          <div className="relative p-10 ">
-            <h1 className="text-6xl  font-bold text-white ">
-              KLINIK <span className="text-yellow-400">HANITA</span>
-            </h1>
-            <p className="text-white text-xs w-3/5 py-3">
-              To provide patient-centered healthcare with excellence in quality,
-              service and access.
-            </p>
-            <Image src={Line} alt="" />
-            <h3 className="text-white py-2 uppercase">Business Hours:</h3>
-            <ul className="text-white text-xxs  w-2/5">
-              <li>MONDAY - FRIDAY (9AM - 10PM)</li>
-              <li>SATURDAY (9AM - 9PM)</li>
-              <li>SUNDAY (10AM -9PM)</li>
-              <li>PUBLIC HOLIDAY (10AM - 9PM)</li>
-            </ul>
-            <a href="https://wa.me/60132401055?">
-              <div className="flex border-2   border-white rounded-full bg-green-600  my-5 items-center gap-2 px-3 py-1 w-fit   ">
-                <div className="text-white text-base lg:text-3xl ">
-                  <AiOutlineWhatsApp />
-                </div>
-                <h2 className="text-white text-xxs  font-bold lg:text-lg  ">
-                  CONTACT US
-                </h2>
+          <div className="relative p-7 md:p-10 md:pb-0  w-fit  md:mx-auto max-w-[1440px] xl:p-20 xl:pb-0">
+            <div className=" flex md:gap-10 items-center xl:gap-20">
+              <div className="  h-fit">
+                <Image
+                  src={Doctor}
+                  alt=""
+                  className=" hidden md:block  md:w-[300px] md:h-full  h-[350px]  xl:w-[450px]  "
+                ></Image>
               </div>
-            </a>
+              <div className="">
+                <h1 className="text-6xl w-fit font-bold text-white  md:text-5xl lg:text-6xl xl:text-8xl">
+                  KLINIK <span className="text-yellow-400">HANITA</span>
+                </h1>
+                <p className="text-white text-xs w-3/5 py-3 sm:w-4/5 xl:text-lg">
+                  To provide patient-centered healthcare with excellence in
+                  quality, service and access.
+                </p>
+                <Image
+                  src={Line}
+                  alt=""
+                  className="  md:h-[50px] xl:h-auto "
+                />
+                <h3 className="text-white py-2 uppercase w-fit xl:text-3xl xl:py-5">
+                  Business Hours:
+                </h3>
+                <ul className="text-white text-xxs  w-2/5 md:w-full xl:text-lg">
+                  <li>MONDAY - FRIDAY (9AM - 10PM)</li>
+                  <li>SATURDAY (9AM - 9PM)</li>
+                  <li>SUNDAY (10AM -9PM)</li>
+                  <li>PUBLIC HOLIDAY (10AM - 9PM)</li>
+                </ul>
+                <a href="https://wa.me/60132401055?">
+                  <div className="inline-flex border-2   border-white rounded-full bg-green-600  my-5  items-center gap-2 px-3 py-1 w-fit xl:my-10 xl:gap-3 xl:py-2 xl:px-5 ">
+                    <div className="text-white text-base xl:text-3xl  ">
+                      <AiOutlineWhatsApp />
+                    </div>
+                    <h2 className="text-white text-xxs  font-bold  lg:text-xs xl:text-lg">
+                      CONTACT US
+                    </h2>
+                  </div>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
