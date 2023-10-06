b9a2c6b2131675a3ae4ad7086edb0b805c98ed7f
Izzat <izzat980420@example.com>
2023-10-07 00:16:11 +0800

    update favicon

--- app/page.tsx
+++ app/page.tsx
@@ -29,24 +29,6 @@ export default function Home() {
       <Head>
         <title>KlinikHanita</title>
         <meta name="description" content="Website Klinik Hanita" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       {/* ----------------------Navbar Section--------------------------------------- */}
